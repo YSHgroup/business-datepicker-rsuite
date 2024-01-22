a691104f04dd8a0997eede2a91bee0cb53ff5b59
YSHgroup <david.dev100@outlook.com>
2024-01-21 19:21:00 -0500

    Add rsuite and implement business datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Container, Header, Content, Sidebar, Footer, Button } from "rsuite";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { CustomProvider } from "rsuite";
+import "./styles/global.less";
+import BusinessDatePicker from "./components/BusinessDatePicker";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CustomProvider theme={theme}>
+      <Container>
+        <Header>Business DatePicker</Header>
+        <Button onClick={toggleTheme}>Toggle Theme</Button>
+        <Content>
+          <BusinessDatePicker></BusinessDatePicker>
+        </Content>
+      </Container>
+    </CustomProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import 'rsuite/dist/rsuite.min.css' // or  'rsuite/styles/index.less'
+import 'rsuite/styles/index.less'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
 )
